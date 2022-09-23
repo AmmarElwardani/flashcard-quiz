ca5c528af5dda937369137a9a70a77485e215a0e
Ammar <ammarelw@gmail.com>
2022-09-23 17:20:26 +0100

    flashcard flip rendering

--- flashcard-quiz/src/App.js
+++ flashcard-quiz/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import FlashcardList from "./FlashcardList";
+import "./app.css"
 
 function App() {
+  const [flashcards, setFlashcards] = useState(FLASHCARD_SAMPLE);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlashcardList flashcards = {flashcards} />
   );
 }
 
+const FLASHCARD_SAMPLE = [
+  {
+    id: 1,
+    question: "What is 2+2 ?",
+    answer: '4',
+    options: [
+      "1",
+      "2",
+      "3",
+      "4",
+    ]
+  },
+  {
+    id: 2,
+    question: "Question 2 ?",
+    answer: 'Answer 1',
+    options: [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4",
+    ]
+  }
+]
 export default App;
