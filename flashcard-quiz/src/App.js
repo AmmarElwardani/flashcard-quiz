161d24c1f7c8ca4cb8ebc71e18161f7e03320a56
Ammar <ammarelw@gmail.com>
2022-09-26 09:43:44 +0100

    adding grid-card css

--- flashcard-quiz/src/App.js
+++ flashcard-quiz/src/App.js
@@ -1,33 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import FlashcardList from "./FlashcardList";
-import "./app.css"
+import "./app.css";
 import axios from "axios";
 function App() {
   const [flashcards, setFlashcards] = useState(FLASHCARD_SAMPLE);
 
-  useEffect(()=>{
-    axios.get("https://opentdb.com/api.php?amount=10")
-      .then(res => {
-        setFlashcards(res.data.results.map((questionItem, index)=>{
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
           const answer = decodeString(questionItem.correct_answer);
-          const options = [...questionItem.incorrect_answers.map((a)=>decodeString(a)), answer];
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
           return {
             id: `${index}-${Date.now()}`,
             question: decodeString(questionItem.question),
             answer: answer,
-            options: options.sort(()=> Math.random() - .5)
-          }
-        }))
-      })
-  }, [])
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
 
-  function decodeString(str){
-    const textArea = document.createElement('textarea');
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
     textArea.innerHTML = str;
     return textArea.value;
   }
   return (
-    <FlashcardList flashcards = {flashcards} />
+    <div className="container">
+      <FlashcardList flashcards={flashcards} />
+    </div>
   );
 }
 
@@ -35,24 +41,14 @@ const FLASHCARD_SAMPLE = [
   {
     id: 1,
     question: "What is 2+2 ?",
-    answer: '4',
-    options: [
-      "1",
-      "2",
-      "3",
-      "4",
-    ]
+    answer: "4",
+    options: ["1", "2", "3", "4"],
   },
   {
     id: 2,
     question: "Question 2 ?",
-    answer: 'Answer 1',
-    options: [
-      "Answer 1",
-      "Answer 2",
-      "Answer 3",
-      "Answer 4",
-    ]
-  }
-]
+    answer: "Answer 1",
+    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+  },
+];
 export default App;
